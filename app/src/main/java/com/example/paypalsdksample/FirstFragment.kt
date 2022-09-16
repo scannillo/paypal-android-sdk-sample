54a0abce56c5294b3a887153828b2fc72c9d3277
Sammy Cannillo <sammyjaynecannillo@gmail.com>
2022-09-16 17:50:04 -0500

    Implement ApproveOrderListener in FirstFragment

--- app/src/main/java/com/example/paypalsdksample/FirstFragment.kt
+++ app/src/main/java/com/example/paypalsdksample/FirstFragment.kt
@@ -8,17 +8,25 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.paypalsdksample.databinding.FragmentFirstBinding
 import com.github.kittinunf.fuel.Fuel
+import com.paypal.android.card.ApproveOrderListener
+import com.paypal.android.card.CardClient
+import com.paypal.android.card.model.CardResult
+import com.paypal.android.core.CoreConfig
+import com.paypal.android.core.Environment
+import com.paypal.android.core.PayPalSDKError
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(), ApproveOrderListener {
 
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    val accessToken: String? = null
 
     // Perform non-UI setup
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +45,8 @@ class FirstFragment : Fragment() {
         Networking.fetchAccessToken { value ->
             binding.textviewFirst.text = value
             println(value)
+
+            configurePayPalSDK()
         }
 
         return binding.root
@@ -53,5 +63,33 @@ class FirstFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun configurePayPalSDK() {
+        val config = CoreConfig(accessToken, environment = Environment.SANDBOX)
+        val cardClient = CardClient(this.requireActivity(), config)
+        cardClient.approveOrderListener = this
+    }
+
+    // IMPLEMENT - ApproveOrderListener Interface
+
+    override fun onApproveOrderCanceled() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onApproveOrderFailure(error: PayPalSDKError) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onApproveOrderSuccess(result: CardResult) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onApproveOrderThreeDSecureDidFinish() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onApproveOrderThreeDSecureWillLaunch() {
+        TODO("Not yet implemented")
     }
 }
